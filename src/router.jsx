7b7f516614f7e8d864ffd8f870dab5b45dae9305
lizuncong <2900170896@qq.com>
2020-02-24 23:22:47 +0800

    stash

--- src/router.jsx
+++ src/router.jsx
@@ -1,53 +1,65 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
+// import loadable from '@loadable/component';
 import {
   HashRouter, Route, Switch, Redirect,
 } from 'react-router-dom';
 
 // import NotFound from 'pages/notfound';
 
-const Login = lazy(() => import('pages/login'));
-const AdminLayout = lazy(() => import('pages/layout/admin'));
-const DetailLayout = lazy(() => import('pages/layout/detail'));
-const Home = lazy(() => import('pages/home'));
-const StoreList = lazy(() => import('pages/store/list'));
-const StoreDetail = lazy(() => import('pages/store/detail'));
-const App = lazy(() => import('./App'));
+// const Login = loadable(() => import(/* webpackChunkName: "login" */'pages/login'));
+// const AdminLayout = loadable(() =>
+// import(/* webpackChunkName: "adminLayout" */'pages/layout/admin'));
+// const DetailLayout = loadable(() =>
+// import(/* webpackChunkName: "detailLayout" */'pages/layout/detail'));
+// const Home = loadable(() =>
+// import(/* webpackChunkName: "home" */'pages/home'));
+// const StoreList = loadable(() =>
+// import(/* webpackChunkName: "storeList" */'pages/store/list'));
+// const StoreDetail = loadable(() =>
+// import(/* webpackChunkName: "storeDetail" */'pages/store/detail'));
+// const App = loadable(() => import(/* webpackChunkName: "app" */'./App'));
+
+import Login from 'pages/login';
+import AdminLayout from 'pages/layout/admin';
+import DetailLayout from 'pages/layout/detail';
+import Home from 'pages/home';
+import StoreList from 'pages/store/list';
+import StoreDetail from 'pages/store/detail';
+import App from './App';
 
 export default class IRouter extends React.Component {
   render() {
     return (
       <HashRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <App>
-            <Switch>
-              <Route path="/login" component={Login} />
-              <Route
-                path="/detail"
-                render={() => (
-                  <DetailLayout>
-                    <Switch>
-                      <Route path="/detail/order/detail" component={Login} />
-                    </Switch>
-                  </DetailLayout>
-                )}
-              />
-              <Route
-                path="/"
-                render={() => (
-                  <AdminLayout>
-                    <Switch>
-                      <Route path="/home" component={Home} />
-                      <Route path="/store/list" component={StoreList} />
-                      <Route path="/store/detail/:storeId?" component={StoreDetail} />
-                      <Redirect to="/home" />
-                      {/* <Route component={NotFound} /> */}
-                    </Switch>
-                  </AdminLayout>
-                )}
-              />
-            </Switch>
-          </App>
-        </Suspense>
+        <App>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route
+              path="/detail"
+              render={() => (
+                <DetailLayout>
+                  <Switch>
+                    <Route path="/detail/order/detail" component={Login} />
+                  </Switch>
+                </DetailLayout>
+              )}
+            />
+            <Route
+              path="/"
+              render={() => (
+                <AdminLayout>
+                  <Switch>
+                    <Route path="/home" component={Home} />
+                    <Route path="/store/list" component={StoreList} />
+                    <Route path="/store/detail/:storeId?" component={StoreDetail} />
+                    <Redirect to="/home" />
+                    {/* <Route component={NotFound} /> */}
+                  </Switch>
+                </AdminLayout>
+              )}
+            />
+          </Switch>
+        </App>
       </HashRouter>
     );
   }
