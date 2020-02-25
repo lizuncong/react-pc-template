ef9f84597b41bd27aa854b319b0d51c3c00d1f51
lizuncong <2900170896@qq.com>
2020-02-25 17:14:17 +0800

    stash

--- src/router.jsx
+++ src/router.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
-// import loadable from '@loadable/component';
+import loadable from '@loadable/component';
 import {
   HashRouter, Route, Switch, Redirect,
 } from 'react-router-dom';
 
 // import NotFound from 'pages/notfound';
 
-// const Login = loadable(() => import(/* webpackChunkName: "login" */'pages/login'));
-// const AdminLayout = loadable(() =>
-// import(/* webpackChunkName: "adminLayout" */'pages/layout/admin'));
-// const DetailLayout = loadable(() =>
-// import(/* webpackChunkName: "detailLayout" */'pages/layout/detail'));
-// const Home = loadable(() =>
-// import(/* webpackChunkName: "home" */'pages/home'));
-// const StoreList = loadable(() =>
-// import(/* webpackChunkName: "storeList" */'pages/store/list'));
-// const StoreDetail = loadable(() =>
-// import(/* webpackChunkName: "storeDetail" */'pages/store/detail'));
-// const App = loadable(() => import(/* webpackChunkName: "app" */'./App'));
+const Login = loadable(() => import(/* webpackChunkName: "login" */'pages/login'));
+const AdminLayout = loadable(() => import(/* webpackChunkName: "adminLayout" */'pages/layout/admin'));
+const DetailLayout = loadable(() => import(/* webpackChunkName: "detailLayout" */'pages/layout/detail'));
+const Home = loadable(() => import(/* webpackChunkName: "home" */'pages/home'));
+const StoreList = loadable(() => import(/* webpackChunkName: "storeList" */'pages/store/list'));
+const StoreDetail = loadable(() => import(/* webpackChunkName: "storeDetail" */'pages/store/detail'));
+const App = loadable(() => import(/* webpackChunkName: "app" */'./App'));
 
-import Login from 'pages/login';
-import AdminLayout from 'pages/layout/admin';
-import DetailLayout from 'pages/layout/detail';
-import Home from 'pages/home';
-import StoreList from 'pages/store/list';
-import StoreDetail from 'pages/store/detail';
-import App from './App';
+// import Login from 'pages/login';
+// import AdminLayout from 'pages/layout/admin';
+// import DetailLayout from 'pages/layout/detail';
+// import Home from 'pages/home';
+// import StoreList from 'pages/store/list';
+// import StoreDetail from 'pages/store/detail';
+// import App from './App';
 
 export default class IRouter extends React.Component {
   render() {
