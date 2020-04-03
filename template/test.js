2e51861afd812da1fe22ef578ed3be5f94d7627f
lizuncong <2900170896@qq.com>
2020-04-03 12:50:53 +0800

    stash

--- template/test.js
+++ template/test.js
@@ -1,16 +1,16 @@
 /*测试文件，需要删除*/
 
-{{#lint}}
+{{#name}}
   const createFunc = () => ({
       console.log('hahah')
   })
-{{/lint}}
+{{/name}}
 
-{{#if_eq build "standalone"}}  // 如果变量build === 'standalone'，则执行里面的语句
+{{#if_eq name "test"}}  // 如果变量name === 'test'，则执行里面的语句
   const a =  1;
 {{/if_eq}}
 
 
-{{#if_or unit e2e}} // 如果变量unit或者变量e2e有值，则执行里面的语句
+{{#if_or name author}} // 如果变量name或者变量author有值，则执行里面的语句
   const b = 2;
 {{/if_or}}
