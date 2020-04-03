346913b5698e241bbbaabf73998b356189d97847
lizuncong <2900170896@qq.com>
2020-04-03 15:20:11 +0800

    stash

--- template/test.js
+++ template/test.js
@@ -1,16 +1,16 @@
 /*测试文件，需要删除*/
 
-{{#name}}
-  const createFunc = () => ({
-      console.log('hahah')
-  })
-{{/name}}
-
-{{#if_eq name "test"}}  // 如果变量name === 'test'，则执行里面的语句
-  const a =  1;
-{{/if_eq}}
-
-
-{{#if_or name author}} // 如果变量name或者变量author有值，则执行里面的语句
-  const b = 2;
-{{/if_or}}
+// {{#name}}
+//   const createFunc = () => ({
+//       console.log('hahah')
+//   })
+// {{/name}}
+//
+// {{#if_eq name "test"}}  // 如果变量name === 'test'，则执行里面的语句
+//   const a =  1;
+// {{/if_eq}}
+//
+//
+// {{#if_or name author}} // 如果变量name或者变量author有值，则执行里面的语句
+//   const b = 2;
+// {{/if_or}}
