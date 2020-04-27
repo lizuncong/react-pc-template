989fb89399ee809aa946601da39aa5e81f9ca4cf
lizuncong <2900170896@qq.com>
2020-04-27 15:12:41 +0800

    actions dispatch派发的说明

--- template/src/pages/redux-demo/actions.js
+++ template/src/pages/redux-demo/actions.js
@@ -1,5 +1,7 @@
 import { changeMoreValue } from './action-creators';
 
+// 如果不使用redux-thunk，redux-saga等这些中间件，那么dispatch派发的只能是一个对象。
+// 即只能dispatch({ type, payload })
 const mapDispatchToProps = (dispatch) => ({
   changeMoreValue: (data) => dispatch(changeMoreValue(data)),
 });
