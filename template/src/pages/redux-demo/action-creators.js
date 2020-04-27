b88dfc1b73a60992ff0c351eb6d706801cd2e682
lizuncong <2900170896@qq.com>
2020-04-27 14:54:11 +0800

    模版添加actionCreator

--- template/src/pages/redux-demo/action-creators.js
+++ template/src/pages/redux-demo/action-creators.js
@@ -1,6 +1,9 @@
+import { createAction } from '../../utils/utils';
 import { CHANGE_MORE_VALUE } from './types';
 
-export const changeMoreValueAction = (payload) => ({
-  type: CHANGE_MORE_VALUE,
-  payload,
-});
+// export const changeMoreValueAction = (payload) => ({
+//   type: CHANGE_MORE_VALUE,
+//   payload,
+// });
+
+export const changeMoreValueAction = createAction(CHANGE_MORE_VALUE, 'payload');
